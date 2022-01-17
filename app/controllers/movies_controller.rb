2644ababa846d4dd9f8cdb5d01eedc64df77d476
Ben <benbperry1@gmail.com>
2022-01-16 16:20:30 -0800

    commit tree 7c5857519dca8086ef95d8a6127bc170283a525f

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,26 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    render json: Movie.create!(movie_params)
+  end
+
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :director, :description, :poster_url, :category, :discount, female_director)
+  end
+
 end
+
+
+t.string "title"
+    t.integer "year"
+    t.integer "length"
+    t.string "director"
+    t.string "description"
+    t.string "poster_url"
+    t.string "category"
+    t.boolean "discount"
+    t.boolean "female_director"
